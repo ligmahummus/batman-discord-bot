24d60a4a60c28a6a9e4d7f492185379f509efefd
Ariel <arielah1997@gmail.com>
2023-12-20 01:33:29 +0200

    fix: typing location and inferring

--- src/lib/rp.service.ts
+++ src/lib/rp.service.ts
@@ -1,32 +1,18 @@
 import { Z_ASCII } from "zlib";
-import { PlayerCount } from "./rp.service.type";
+import { PlayerCount, RPServer, RPServerResponse } from "./rp.service.type";
 
 /**
  * Returns the current and total player count from the RP website.
  */
 export async function playerCount(serverIp: string): Promise<RPServer | null> {
   try {
-    const serversMasterList: Promise<RPServerResponse> = await fetch(
+    const serversMasterList: RPServerResponse = await fetch(
       "https://cdn.rage.mp/master"
     ).then((res) => res.json());
-    const server = serversMasterList[serverIp];
+    const server = serversMasterList[serverIp as keyof RPServerResponse];
+    if (!server) throw new Error("Server not found");
     return server;
   } catch (error) {
     return null;
   }
 }
-// play.eclipse-rp.lt:22010
-
-interface RPServerResponse {
-  [key: string]: RPServer;
-}
-
-type RPServer = {
-  name: string;
-  players: number;
-  peak: number;
-  maxplayers: number;
-  lang: string;
-  url: string;
-  gamemode: string;
-};
--- src/lib/rp.service.type.ts
+++ src/lib/rp.service.type.ts
@@ -2,3 +2,17 @@ export type PlayerCount = {
   current: string;
   total: string;
 };
+
+export interface RPServerResponse {
+  [key: string]: RPServer;
+}
+
+export type RPServer = {
+  name: string;
+  players: number;
+  peak: number;
+  maxplayers: number;
+  lang: string;
+  url: string;
+  gamemode: string;
+};
