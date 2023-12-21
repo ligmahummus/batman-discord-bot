df8af2c536b82849b416663050d70bd07fef9d8f
Ariel <arielah1997@gmail.com>
2023-12-21 12:29:33 +0200

    feat: new aws adjustments

--- src/bot.ts
+++ src/bot.ts
@@ -4,44 +4,46 @@ import { blockQuote, bold } from "@discordjs/formatters";
 import { clientLogger } from "./lib/util";
 import { playerCount } from "./lib/rp.service";
 
-const client = new Client(clientOptions);
+export function main() {
+  const client = new Client(clientOptions);
 
-/**
- * Event listener for when the client is ready.
- */
-client.on("ready", () => {
-  clientLogger(`Logged in as ${client.user?.tag}!`);
-});
+  /**
+   * Event listener for when the client is ready.
+   */
+  client.on("ready", () => {
+    clientLogger(`Logged in as ${client.user?.tag}!`);
+  });
 
-/**
- * Event listener for client messages for ANY channel.
- */
-client.on("messageCreate", async (msg) => {
-  const content = msg.content.toLowerCase();
-  if (content === "ping") {
-  } else if (content === "players") {
-    msg.reply("Fetching player count...");
-    playerCount("play.eclipse-rp.net:22005")
-      .then((server) => {
-        if (server) {
-          msg.reply(
-            blockQuote(
-              `${bold("Server:")} ${
-                server.name
-              } - ${server.gamemode.toUpperCase()}\n${bold(
-                "Current players:"
-              )} ${server.players} / ${server.maxplayers}`
-            )
-          );
-        } else {
-          msg.reply("Failed to fetch player count.");
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        msg.reply("Errored while fetching player count.");
-      });
-  }
-});
+  /**
+   * Event listener for client messages for ANY channel.
+   */
+  client.on("messageCreate", async (msg) => {
+    const content = msg.content.toLowerCase();
+    if (content === "ping") {
+    } else if (content === "players") {
+      msg.reply("Fetching player count...");
+      playerCount("play.eclipse-rp.net:22005")
+        .then((server) => {
+          if (server) {
+            msg.reply(
+              blockQuote(
+                `${bold("Server:")} ${
+                  server.name
+                } - ${server.gamemode.toUpperCase()}\n${bold(
+                  "Current players:"
+                )} ${server.players} / ${server.maxplayers}`
+              )
+            );
+          } else {
+            msg.reply("Failed to fetch player count.");
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          msg.reply("Errored while fetching player count.");
+        });
+    }
+  });
 
-client.login(botConfig.token);
+  client.login(botConfig.token);
+}
