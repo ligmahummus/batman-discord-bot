afeb88146e6696b092716a6e08ca1e26ea0a94f2
Ariel <arielah1997@gmail.com>
2024-02-09 16:54:47 +0200

    fix: remove dev support

--- src/lib/bot/config.ts
+++ src/lib/bot/config.ts
@@ -10,21 +10,21 @@ let clientSecret: string = "";
 let guildId: string = "";
 
 // Set the bot token and client ID based on the environment.
-if (process.env.NODE_ENV === "production") {
-  clientLogger("Running in production mode.");
+// if (process.env.NODE_ENV === "production") {
+//   clientLogger("Running in production mode.");
 
-  token = process.env.BOT_TOKEN || "";
-  clientId = process.env.BOT_CLIENT_ID || "";
-  clientSecret = process.env.BOT_SECRET_ID || "";
-  guildId = process.env.BOT_SERVER_ID || "";
-} else {
-  clientLogger("Running in development mode.");
+token = process.env.BOT_TOKEN || "";
+clientId = process.env.BOT_CLIENT_ID || "";
+clientSecret = process.env.BOT_SECRET_ID || "";
+guildId = process.env.BOT_SERVER_ID || "";
+// } else {
+//   clientLogger("Running in development mode.");
 
-  token = process.env.BOT_TOKEN_DEV || "";
-  clientId = process.env.BOT_CLIENT_ID_DEV || "";
-  clientSecret = process.env.BOT_SECRET_ID_DEV || "";
-  guildId = process.env.BOT_SERVER_ID_DEV || "";
-}
+//   token = process.env.BOT_TOKEN_DEV || "";
+//   clientId = process.env.BOT_CLIENT_ID_DEV || "";
+//   clientSecret = process.env.BOT_SECRET_ID_DEV || "";
+//   guildId = process.env.BOT_SERVER_ID_DEV || "";
+// }
 
 /**
  * Bot main configuration object.
