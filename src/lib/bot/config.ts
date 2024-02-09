c95b466467f1d7f2c2593e38955dde9d726f0eae
Ariel <arielah1997@gmail.com>
2024-02-09 14:40:28 +0200

    ci: log env file into terminal for debugging

--- src/lib/bot/config.ts
+++ src/lib/bot/config.ts
@@ -4,14 +4,36 @@ import { clientLogger } from "../utils/util";
 // Required to load .env file.
 dotenv.config();
 
+let token: string = "";
+let clientId: string = "";
+let clientSecret: string = "";
+let guildId: string = "";
+
+// Set the bot token and client ID based on the environment.
+if (process.env.NODE_ENV === "production") {
+  clientLogger("Running in production mode.");
+
+  token = process.env.BOT_TOKEN || "";
+  clientId = process.env.BOT_CLIENT_ID || "";
+  clientSecret = process.env.BOT_SECRET_ID_DEV || "";
+  guildId = process.env.BOT_SERVER_ID || "";
+} else {
+  clientLogger("Running in development mode.");
+
+  token = process.env.BOT_TOKEN_DEV || "";
+  clientId = process.env.BOT_CLIENT_ID_DEV || "";
+  clientSecret = process.env.BOT_SECRET_ID_DEV || "";
+  guildId = process.env.BOT_SERVER_ID_DEV || "";
+}
+
 /**
  * Bot main configuration object.
  */
 export const botConfig = {
-  token: process.env.BOT_TOKEN || "",
-  clientId: process.env.BOT_CLIENT_ID || "",
-  clientSecret: process.env.BOT_CLIENT_SECRET || "",
-  guildId: process.env.BOT_SERVER_ID || "",
+  token: token,
+  clientId: clientId,
+  clientSecret: clientSecret,
+  guildId: guildId,
   appId: process.env.BOT_APP_ID || "",
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
-import { Bot } from "./lib/bot/bot";
+import { Bot } from "./lib/bot/bot-client";
 import { clientOptions } from "./lib/bot/config";
 import { clientLogger } from "./lib/utils/util";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Main source of the bot instance.
 export const clientBot: Bot = new Bot(clientOptions);
+export let name = "dude";
 
 const PORT = process.env.PORT || 42069;
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/name", (req, res) => {
+  res.send(name);
+});
+
+app.post("/name", (req, res) => {
+  name = req.body.name;
+  res.send("name changed to " + name);
 });
 
 app.listen(+PORT, () => {
