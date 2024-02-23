31679c1a78e2f701f9c71fc781fcd2d6c2095771
Ariel <arielah1997@gmail.com>
2024-02-23 18:31:28 +0200

    style: add info sections and style

--- src/lib/audit/model/audit-model.ts
+++ src/lib/audit/model/audit-model.ts
@@ -1,15 +1,18 @@
 import { model, models, Schema } from "mongoose";
 
-const auditSchema = new Schema({
-  players: {
-    type: Number,
-    required: true,
+const auditSchema = new Schema(
+  {
+    players: {
+      type: Number,
+      required: true,
+    },
+    time: {
+      type: Date,
+      required: true,
+    },
   },
-  time: {
-    type: Date,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const auditModel = models.Audit || model("Audit", auditSchema);
 
